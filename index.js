364da2bc68a6bbb758a43b4676dd41d80952baf4
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-13 20:57:40 -0600

    commit tree 7179ef6b1179fac3679418248f0877d3703f7e0a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity2 = "go to the office"){
+    return `This Monday, I will ${activity2}.`;
+}
+function wrapAdjective(highlight="*"){
+    return function (parameter ="special"){
+        return `You are ${highlight}${parameter}${highlight}!`;
+    
+    }
+}
+wrapAdjective("%")("a dedicated programmer")
